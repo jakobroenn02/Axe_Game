3d65fc4debd1fb97c2c9ff690da6718ac164dd99
Jakob <66146289+jakobroenn02@users.noreply.github.com>
2021-08-07 17:26:44 +0200

    Add files via upload

--- vscode-template-master/axe_game.cpp
+++ vscode-template-master/axe_game.cpp
@@ -6,35 +6,84 @@ int main()
     int width = 800;
     int wide = 450;
     InitWindow(width, wide, "Jakob");
+
     int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+    // circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
 
     int axe_x{400};
     int axe_y{0};
-
+    int axe_length{50};
+    int direction{10};
+    //axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
     SetTargetFPS(60);
+    bool collision_with_axe =  
+                        (b_axe_y >= u_circle_y) && 
+                        (u_axe_y <= b_circle_y) && 
+                        (r_axe_x >= l_circle_x) && 
+                        (l_axe_x <= r_circle_x);
+
     while (WindowShouldClose() == false){
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-        //making movement
-        DrawCircle(circle_x,circle_y,30,BLUE);
-        DrawRectangle(axe_x,axe_y, 50, 50, RED);
-
-        axe_y += 10;
-
-        if (IsKeyDown(KEY_D) && circle_x < 800)
+         if (collision_with_axe == true)
         {
-            circle_x += 10;
-        }
-
-        if (IsKeyDown(KEY_A) && circle_x > 0)
-        {
-            circle_x -= 10;
+            DrawText("Game Over!", 400,200, 20 ,RED);
 
         }
 
+            else{
+
+                // update edges
+                l_circle_x = circle_x - circle_radius;
+                r_circle_x = circle_x + circle_radius;
+                u_circle_y = circle_y + circle_radius;
+                b_circle_y = circle_y + circle_radius;
+                l_axe_x = axe_x;
+                r_axe_x = axe_x + axe_length;
+                u_axe_y = axe_y;
+                b_axe_y = axe_y + axe_length;
+                // update collision with axe
+                collision_with_axe =  
+                        (b_axe_y >= u_circle_y) && 
+                        (u_axe_y <= b_circle_y) && 
+                        (r_axe_x >= l_circle_x) && 
+                        (l_axe_x <= r_circle_x);
+                //movement
+                DrawCircle(circle_x,circle_y,circle_radius,BLUE);
+                DrawRectangle(axe_x,axe_y, axe_length, axe_length, RED);
+                /* code */
+            
+                axe_y += direction;
+                if (axe_y > wide || axe_y < 0)
+                {
+                direction = -direction;
+                }
+        
+                if (IsKeyDown(KEY_D) && circle_x < width)
+                {
+                    circle_x += 10;
+                }
+
+                if (IsKeyDown(KEY_A) && circle_x > 0)
+                {
+                    circle_x -= 10;
+
+                }
+        
+             }
 
         EndDrawing();
 
